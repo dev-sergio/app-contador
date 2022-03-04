2841932f422c905e9a564656340a76f6e45a61a0
Sergio Junior <sergio.junior@beltsysplus.com.br>
2022-03-04 17:23:35 -0300

    Atualizando componentes da tela

---
+++ android/app/src/main/kotlin/br/com/sergiojr/contador/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sergiojr.contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+﻿import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  void decrement() {
+    print('decrement');
+  }
+
+  void increment() {
+    print('increment');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'PODE ENTRAR',
+            style: TextStyle(
+                fontSize: 35, color: Colors.white, fontWeight: FontWeight.w700),
+          ),
+          const Text(
+            '0',
+            style: TextStyle(
+                fontSize: 100, color: Colors.red, fontWeight: FontWeight.w400),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  //padding: const EdgeInsets.all(32)
+                  primary: Colors.black,
+                ),
+                onPressed: decrement,
+                child: const Text(
+                  'Saiu',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  //padding: const EdgeInsets.all(32)
+                  primary: Colors.black,
+                ),
+                onPressed: increment,
+                child: const Text(
+                  'Entrou',
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}

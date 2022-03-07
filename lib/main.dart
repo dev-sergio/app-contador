2667ab93bf8957bb14c518faa42c96333b44b9c0
Sergio Junior <sergio.junior@beltsysplus.com.br>
2022-03-07 11:45:23 -0300

    Finalizado o app

--- lib/main.dart
+++ lib/main.dart
@@ -10,71 +10,106 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
   void decrement() {
-    print('decrement');
+    setState(() {
+      count--;
+    });
   }
 
   void increment() {
-    print('increment');
+    setState(() {
+      count++;
+    });
   }
+
+  int lotacaoMaxima = 20;
+  bool get isFull => count >= lotacaoMaxima;
+  bool get isEmpty => count == 0;
+  int get podeEntrar => lotacaoMaxima - count;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'PODE ENTRAR',
-            style: TextStyle(
-                fontSize: 35, color: Colors.white, fontWeight: FontWeight.w700),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/restaurante.jpg'),
+              fit: BoxFit.cover
           ),
-          const Text(
-            '0',
-            style: TextStyle(
-                fontSize: 100, color: Colors.red, fontWeight: FontWeight.w400),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  //padding: const EdgeInsets.all(32)
-                  primary: Colors.black,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? 'LOTADO' : 'PODE ENTRAR ($podeEntrar)',
+              style: const TextStyle(
+                  fontSize: 35, color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(40),
+                child: Text(
+                  '$count',
+                  style: TextStyle(
+                      fontSize: 100, color: isFull ? Colors.red : Colors.white, fontWeight: FontWeight.w400),
+                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    //padding: const EdgeInsets.all(32)
+                    primary: Colors.black,
+                  ),
+                  onPressed: isEmpty ? null : decrement,
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
                 ),
-                onPressed: decrement,
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
+                const SizedBox(width: 32),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)
+                    ),
+                    backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    //padding: const EdgeInsets.all(32)
+                    primary: Colors.black,
+                  ),
+                  onPressed: isFull? null : increment,
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
                 ),
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  //padding: const EdgeInsets.all(32)
-                  primary: Colors.black,
-                ),
-                onPressed: increment,
-                child: const Text(
-                  'Entrou',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
